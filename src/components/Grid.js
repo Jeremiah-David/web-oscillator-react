93a8e411b39dc32d247c2a58c0e6a0d086f2afdc
Blake DeGraw <bbdegraw@gmail.com>
2021-04-08 11:45:38 -0700

    moved osc to Grid.js

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,9 +1,79 @@
-function Grid () {
+function Grid() {
+
+    let oscillatorX, oscillatorY
+
+    let oscX = {
+        type: "sine",
+        frequency: 200,
+        playing: false
+    }
+
+    let oscY = {
+        type: oscX.type,
+        frequency: 200 * 1.01,
+        playing: false
+    }
+
+    const audioContext = new AudioContext()
+
+    function playX() {
+        if (oscX.playing) {
+            oscillatorX.stop()
+            oscX.playing = false
+        } else {
+            oscillatorX = audioContext.createOscillator()
+            oscillatorX.type = oscX.type
+            oscillatorX.frequency.setValueAtTime(oscX.frequency, audioContext.currentTime)
+            oscillatorX.connect(audioContext.destination)
+            oscillatorX.start()
+            oscX.playing = true
+        }
+    }
+
+    function playY() {
+        if (oscY.playing) {
+            oscillatorY.stop()
+            oscY.playing = false
+        } else {
+            oscillatorY = audioContext.createOscillator()
+            oscillatorY.type = oscY.type
+            oscillatorY.frequency.setValueAtTime(oscY.frequency, audioContext.currentTime)
+            oscillatorY.connect(audioContext.destination)
+            oscillatorY.start()
+            oscY.playing = true
+        }
+    }
+
+    function setSine() {
+        oscX.type = "sine"
+        oscY.type = "sine"
+    }
+
+    function setTriangle() {
+        oscX.type = "triangle"
+        oscY.type = "triangle"
+    }
+
+    function setSquare() {
+        oscX.type = "square"
+        oscY.type = "square"
+    }
+
+    function setSawtooth() {
+        oscX.type = "sawtooth"
+        oscY.type = "sawtooth"
+    }
+
     return (
         <div className="osc">
             <h1>Where the music happens</h1>
-        </div> 
+            <button type="button" onClick={() => { playX(); playY() }}>play/pause</button><br></br>
+            <button type="button" onClick={setSine}>sine</button><br></br>
+            <button type="button" onClick={setTriangle}>triangle</button><br></br>
+            <button type="button" onClick={setSquare}>square</button><br></br>
+            <button type="button" onClick={setSawtooth}>sawtooth</button><br></br>
+        </div>
     )
 }
 
-export default Grid 
+export default Grid

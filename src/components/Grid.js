0c7bae2cbde7f486fe22070eea3f4c73a269c5b5
Jeremiah <dancesfire@gmail.com>
2021-04-08 11:51:40 -0700

    finger axis to grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,7 @@
-function Grid () {
+function Grid (props) {
     return (
         <div className="osc">
+            {console.log('grid', props)}
             <h1>Where the music happens</h1>
         </div> 
     )
